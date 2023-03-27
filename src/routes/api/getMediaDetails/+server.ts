c08fcb77ef53ba4add481e350856eec67321bc23
RCLemos-PMOGA <78752138+RCLemos-PMOGA@users.noreply.github.com>
2023-03-27 13:45:00 -0300

    Update +server.ts
    add omdbapi key

--- src/routes/api/getMediaDetails/+server.ts
+++ src/routes/api/getMediaDetails/+server.ts
@@ -1,9 +1,9 @@
-import { OMDB_API_KEY } from '$env/static/private';
+//import { OMDB_API_KEY } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request }: { request: any }) {
 	const { title } = await request.json();
-	const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${title}`;
+	const url = `http://www.omdbapi.com/?apikey=$1e8c7116&t=${title}`;
 
 	const res = await fetch(url);
 	const details = await res.json();
