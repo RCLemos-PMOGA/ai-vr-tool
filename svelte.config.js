8793b23647c6fa95656a023b7c11060eb4b875e9
RCLemos-PMOGA <78752138+RCLemos-PMOGA@users.noreply.github.com>
2023-03-29 17:41:47 -0300

    add city

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+let city = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
