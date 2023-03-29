70daaf3be83f2e37f850a599628fc636ccd770b3
RCLemos-PMOGA <78752138+RCLemos-PMOGA@users.noreply.github.com>
2023-03-29 13:34:33 -0300

    logo in home page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -7,7 +7,7 @@
 >
 	<button on:click class="flex items-center mb-4 md:mb-0 text-white/70">
 		<TvIcon />
-		<div class="text-2xl md:text-xl font-bold ml-2">watchthis.dev</div>
+		<div class="text-2xl md:text-xl font-bold ml-2"><img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3267809/settings_images/ee6P8Rj2Q8eXfX7Qa2ib_PMOGA_Logo_-_Horizontal_-_Dark_Backgroung.png" /></div>
 	</button>
 	<a
 		class="bg-white/20 hover:bg-white/50 shadow text-white hover:text-pink-600 font-medium rounded p-2 text-sm flex items-center"
