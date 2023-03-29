40d2f5d1853b2a6ecf22b0f2746fd61ffcac041c
RCLemos-PMOGA <78752138+RCLemos-PMOGA@users.noreply.github.com>
2023-03-29 13:37:21 -0300

    hoe logo width update

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,7 +6,6 @@
 	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mb-12 border-b border-white/20"
 >
 	<button on:click class="flex items-center mb-4 md:mb-0 text-white/70">
-		<TvIcon />
-		<div class="text-2xl md:text-xl font-bold ml-2"><img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3267809/settings_images/ee6P8Rj2Q8eXfX7Qa2ib_PMOGA_Logo_-_Horizontal_-_Dark_Backgroung.png" /></div>
+		<div class="text-2xl md:text-xl font-bold ml-2"><img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3267809/settings_images/ee6P8Rj2Q8eXfX7Qa2ib_PMOGA_Logo_-_Horizontal_-_Dark_Backgroung.png" width="200px"/></div>
 	</button>
 </div>
