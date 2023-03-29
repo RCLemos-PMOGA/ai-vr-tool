030c08ffe0518279242f40318d74fb1d806a7942
RCLemos-PMOGA <78752138+RCLemos-PMOGA@users.noreply.github.com>
2023-03-29 17:06:12 -0300

    insert city in the global scop

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -6,30 +6,50 @@
 		<br class="hidden md:block" /> with Open AI
 	</div>
 	<hr>
-	<script>
-		import { firestore } from './firebase.js'
-		let city = ''
-		let resultados = ''
-	  
-		async function searchByCity() {
-		  const querySnapshot = await firestore.collection('Exemplo').where('CITY', '==', city).get()
-		  resultados = ''
-		  querySnapshot.forEach((doc) => {
-			resultados += `Cidade: ${doc.data().CITY}, Estado: ${doc.data().STATE}<br>`
-		  })
-		  if (resultados === '') {
-			resultados = 'Nenhum resultado encontrado.'
-		  }
-		  document.getElementById('resultados').innerHTML = resultados
-		}
-	  </script>
-	  
-	  <label for="city">Digite o nome da cidade:</label>
-	  <input type="text" id="city" bind:value={city}>
-	  
-	  <button on:click={searchByCity}>Pesquisar por cidade</button>
-	  
-	  <div id="resultados"></div>
+	<!-- Arquivo App.svelte -->
+
+<script>
+	import { initializeApp } from 'firebase/app'
+	import { getFirestore } from 'firebase/firestore'
+	let city = ''
+	let resultados = ''
+	
+	// Inicializa o Firebase
+	const firebaseConfig = {
+		apiKey: "AIzaSyBfd--6fT0CJCPOIa4SOQVDTmiQBBhUQxM",
+    authDomain: "pmoflix-deb5a.firebaseapp.com",
+    databaseURL: "https://pmoflix-deb5a-default-rtdb.firebaseio.com",
+    projectId: "pmoflix-deb5a",
+    storageBucket: "pmoflix-deb5a.appspot.com",
+    messagingSenderId: "477067952595",
+    appId: "1:477067952595:web:a8b3ed6a1b200fcbfbef26",
+    measurementId: "G-3B5RF84JCE"
+	}
+	const app = initializeApp(firebaseConfig)
+	const firestore = getFirestore(app)
+	
+	console.log(city) // Verifica se a variável city está sendo declarada corretamente
+	
+	// Define a função de pesquisa por cidade
+	async function searchByCity() {
+	  const querySnapshot = await firestore.collection('Exemplo').where('CITY', '==', city).get()
+	  resultados = ''
+	  querySnapshot.forEach((doc) => {
+		resultados += `Cidade: ${doc.data().CITY}, Estado: ${doc.data().STATE}<br>`
+	  })
+	  if (resultados === '') {
+		resultados = 'Nenhum resultado encontrado.'
+	  }
+	  document.getElementById('resultados').innerHTML = resultados
+	}
+  </script>
+  
+  <label for="city">Digite o nome da cidade:</label>
+  <input type="text" id="city" bind:value={city}>
+  
+  <button on:click={searchByCity}>Pesquisar por cidade</button>
+  
+  <div id="resultados"></div>
 		<br>
 	<hr>
 	<div class="flex flex-wrap items-center mr-auto">
