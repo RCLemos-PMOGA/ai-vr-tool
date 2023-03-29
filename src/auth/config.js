54ad5be8ffc0d3411cd014c9b7257f01969e8dbb
RCLemos-PMOGA <78752138+RCLemos-PMOGA@users.noreply.github.com>
2023-03-29 16:31:31 -0300

    add the firebase on svelte app

---
+++ src/auth/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBfd--6fT0CJCPOIa4SOQVDTmiQBBhUQxM",
+    authDomain: "pmoflix-deb5a.firebaseapp.com",
+    databaseURL: "https://pmoflix-deb5a-default-rtdb.firebaseio.com",
+    projectId: "pmoflix-deb5a",
+    storageBucket: "pmoflix-deb5a.appspot.com",
+    messagingSenderId: "477067952595",
+    appId: "1:477067952595:web:a8b3ed6a1b200fcbfbef26",
+    measurementId: "G-3B5RF84JCE"
+  };
--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -5,6 +5,23 @@
 		Get curated show <br class="hidden md:block" /> and movie recommendations
 		<br class="hidden md:block" /> with Open AI
 	</div>
+	<hr>
+	<script>
+		import { firestore } from './firebase.js'
+		
+		async function searchByCity() {
+		  const city = prompt('Digite o nome da cidade:')
+		  const querySnapshot = await firestore.collection('Exemplo').where('CITY', '==', city).get()
+		  querySnapshot.forEach((doc) => {
+			console.log(doc.id, " => ", doc.data())
+			// ou, por exemplo:
+			// this.resultados = doc.data() (se você estiver usando o Svelte)
+		  })
+		}
+		</script>
+		
+		<button on:click={searchByCity}>Pesquisar por cidade</button>
+	<hr>
 	<div class="flex flex-wrap items-center mr-auto">
 		<button
 			on:click
